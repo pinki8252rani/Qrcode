9326f085686c9edb1202d92172eb023c0d92557e
pinki8252rani <138314899+pinki8252rani@users.noreply.github.com>
2023-10-06 13:20:59 +0530

    Add files via upload

---
+++ QR.py
@@ -0,0 +1,3 @@
+import qrcode as qr
+img=qr.make("https://github.com/pinki8252rani")
+img.save("pinki_github.png")
---
+++ qr_1.py
@@ -0,0 +1,9 @@
+import qrcode
+from PIL import Image
+qr=qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_H,
+                 box_size=10,border=4,)
+qr.add_data("https://github.com/pinki8252rani")
+qr.make(fit=True)
+img=qr.make_image(fill_color="black",back_color="white")
+img.save("pinkigithub_qrcode.png")
+
